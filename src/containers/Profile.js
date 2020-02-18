07d829ea3ad60ef4bc7c8be0fd491c3f2d9d8351
Kenneth Ofosu <kofosu89@gmail.com>
2020-02-18 17:14:26 -0500

    edit Profile container to make use of fetched data from GH API

--- src/containers/Profile.js
+++ src/containers/Profile.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Link from '../components/Link/Link';
 
 class Profile extends Component {
   constructor() {
@@ -29,17 +30,17 @@ class Profile extends Component {
     }
 
     return (
-      <div>
-        <ul>
-          <li>avatar_url: {data.avatar_url}</li>
-          <li>html_url: {data.html_url}</li>
-          <li>repos_url: {data.repos_url}</li>
-          <li>name: {data.name}</li>
-          <li>company: {data.company}</li>
-          <li>location: {data.location}</li>
-          <li>email: {data.email}</li>
-          <li>bio: {data.bio}</li>
-        </ul>
+      <div className='Profile-container'>
+        <img className='Profile-avatar' src={data.avatar_url} alt='avatar' />
+          <ul>
+            <li><strong>html_url:</strong> <Link url={data.html_url} title='Github URL' /></li>
+            <li><strong>repos_url:</strong> {data.repos_url}</li>
+            <li><strong>name:</strong> {data.name}</li>
+            <li><strong>company:</strong> {data.company}</li>
+            <li><strong>location:</strong> {data.location}</li>
+            <li><strong>email:</strong> {data.email}</li>
+            <li><strong>bio:</strong> {data.bio}</li>
+         </ul>
       </div>
     );
   }
