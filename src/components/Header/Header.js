9a455a609df344956aa7599cb2004425cc5ae4f8
Kenneth Ofosu <kofosu89@gmail.com>
2020-02-18 17:08:59 -0500

    edit Header component to replace boilerplate code with github logo and new title

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,14 +1,13 @@
 import React from 'react';
-import Link from '../Link/Link';
+import logo from '../../GitHub-Mark-Light-64px.png';
 import './Header.css';
 
-const Header = ({logo}) => (
+const Header = () => (
   <header className='App-header'>
     < img src={logo} className='App-logo' alt='logo' />
-    <p>
-      Edit <code> src/App.js </code> and save to reload.
-    </p>
-    <Link url = 'https://reactjs.org' title='Learn React' />
+    <h1>
+      My Github Portfolio
+    </h1>
   </header>
 );
 export default Header;
