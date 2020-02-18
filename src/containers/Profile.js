6b34f29d2d4076779b0542b8d4c0a8befad38011
Kenneth Ofosu <kofosu89@gmail.com>
2020-02-18 17:28:45 -0500

    edit Profile container to use List component

--- src/containers/Profile.js
+++ src/containers/Profile.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Link from '../components/Link/Link';
+import List from '../components/List/List';
 import './Profile.css';
 
 class Profile extends Component {
@@ -30,18 +31,20 @@ class Profile extends Component {
       return <div>Loading...</div>
     }
 
+    const items = [
+      { label: 'html_url', value: <Link url={data.html_url} title='Github URL' /> },
+      { label: 'repos_url', value: data.repos_url },
+      { label: 'name', value: data.name},
+      { label: 'company', value: data.company },
+      { label: 'location', value: data.location },
+      { label: 'email', value: data.email },
+      { label: 'bio', value: data.bio }
+    ]
+
     return (
       <div className='Profile-container'>
         <img className='Profile-avatar' src={data.avatar_url} alt='avatar' />
-          <ul>
-            <li><strong>html_url:</strong> <Link url={data.html_url} title='Github URL' /></li>
-            <li><strong>repos_url:</strong> {data.repos_url}</li>
-            <li><strong>name:</strong> {data.name}</li>
-            <li><strong>company:</strong> {data.company}</li>
-            <li><strong>location:</strong> {data.location}</li>
-            <li><strong>email:</strong> {data.email}</li>
-            <li><strong>bio:</strong> {data.bio}</li>
-         </ul>
+          <List items = {items} />
       </div>
     );
   }
