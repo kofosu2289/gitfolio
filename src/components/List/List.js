8e85c357c00a90ec1c203e5e314e062ccb31f739
Kenneth Ofosu <kofosu89@gmail.com>
2020-02-18 18:43:11 -0500

    edit List component to use styled components

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,13 +1,32 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const ListWrapper = styled.ul`
+  list-style: none;
+  text-align: left;
+  padding: 0;
+`;
+
+const ListItem = styled.li`
+  display: flex;
+  justify-content: space-between;
+`;
+
+
+const label = styled.span`
+  font-weight: strong;
+`;
 
 const List = ({items}) => (
-  <ul>
-    {items.map(item =>
-      <li key={item.label}>
-        <strong>{item.label}</strong>{item.value}
-      </li>
-    )}
-  </ul>
+  
+    <ListWrapper>
+      {items.map(item =>
+        <ListItem key={item.label}>
+          <Label>{item.label}</Label>{item.value}
+        </ListItem>
+      )}
+    </ListWrapper>
+  
 );
 
 export default List;
