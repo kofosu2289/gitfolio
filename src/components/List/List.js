35bd914adf4b627882e446f661b8246cfca0bc19
Kenneth Ofosu <kofosu89@gmail.com>
2020-02-18 17:39:30 -0500

    edit List component to use props passed from Profile container

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,7 +1,13 @@
 import React from 'react';
 
-const List = () => (
-  <ul></ul>
+const List = ({items}) => (
+  <ul>
+    {items.map(item =>
+      <li key={item.label}>
+        <strong>{item.label}</strong>{item.value}
+      </li>
+    )}
+  </ul>
 );
 
 export default List;
