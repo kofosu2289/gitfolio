3535e5649ef9f43ffc73081fb04bb584598b391f
Kenneth Ofosu <kofosu89@gmail.com>
2020-02-18 19:05:01 -0500

    edit List component to use props passed from Profile container

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import styled from 'styled-components';
 
+const Title = styled.h2`
+  padding: 10px 0;
+  border-bottom: 1px solid lightGrey;
+`;
+
 const ListWrapper = styled.ul`
   list-style: none;
   text-align: left;
@@ -17,8 +22,9 @@ const Label = styled.span`
   font-weight: bold;
 `;
 
-const List = ({items}) => (
-  
+const List = ({items, title}) => (
+  <>
+    <Title>{title}</Title>
     <ListWrapper>
       {items.map(item =>
         <ListItem key={item.label}>
@@ -26,7 +32,7 @@ const List = ({items}) => (
         </ListItem>
       )}
     </ListWrapper>
-  
+  </>
 );
 
 export default List;
